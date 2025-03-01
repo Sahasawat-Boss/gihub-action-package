83bac37d777ae31c9770a049081315f73a0ebe91
Boss <sahasawat.rk@gmail.com>
2025-03-01 08:41:52 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//only 1 function here
+
+const sayHello = function (name) {
+    console.log("hello" + name + "!"),
+    console.log("This is my first NPM package")
+}
+
+module.export = {
+    sayHello
+}
